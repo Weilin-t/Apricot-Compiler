75d07b70edd3910d001b4a178592f01aed3c2fbc
Tan Weilin <weilin.t@digipen.edu>
2022-09-20 02:26:17 +0800

    empty solution created

---
+++ Melon-Compiler/Melon-Compiler/main.cpp
@@ -0,0 +1,20 @@
+/*****************************************************************/
+/*!
+\file   main.cpp
+
+\author Tan Weilin, Weilin
+\par    Email: weilin.t@digipen.edu
+\date   20 September 2022
+\brief  This file is the starting point of the compiler
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+*/ 
+/*********************************************************************/
+
+int main(int argc, const char* argv[])
+{
+
+	return 0;
+}
